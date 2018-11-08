ffa9dd1bca48626fcf18cb7876b85906c4863cf1
Julio Venero Loayza <venerojulio@gmail.com>
2018-11-07 20:17:16 -0500

    feat: The search works correctly when a book does not have a thumbnail or an author

--- src/Book.js
+++ src/Book.js
@@ -10,7 +10,10 @@ class Book extends Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
+          <div className="book-cover" 
+          style={{ width: 128, height: 193, 
+                  backgroundImage: `url(${(this.props.book.imageLinks) ? this.props.book.imageLinks.thumbnail : ""})` }}>
+          </div>
           <div className="book-shelf-changer">
             <select defaultValue={this.props.book.shelf} onChange={(e) => this.changeShelf(this.props.book, e.target.value)}>
               <option value="move" disabled>Move to...</option>
@@ -22,7 +25,7 @@ class Book extends Component {
           </div>
         </div>
         <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">{this.props.book.authors}</div>
+        <div className="book-authors">{ (this.props.book.authors) ? this.props.book.authors.join(', ') : "" }</div>
       </div>
     )
   }
